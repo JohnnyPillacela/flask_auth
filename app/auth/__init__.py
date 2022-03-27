02c5f972e87ed812e2c554baef0547f060cda2eb
Johnny Pillacela <johnnybelesaca@gmail.com>
2022-03-27 15:22:02 -0400

    only validating form if request is POST

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -14,20 +14,21 @@ def login():
     form = login_form()
     if current_user.is_authenticated:
         return redirect(url_for('auth.dashboard'))
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
-            return redirect(url_for('auth.login'))
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            user = User.query.filter_by(email=form.email.data).first()
+            if user is None or not user.check_password(form.password.data):
+                flash('Invalid username or password')
+                return redirect(url_for('auth.login'))
+            else:
+                user.authenticated = True
+                db.session.add(user)
+                db.session.commit()
+                login_user(user)
+                flash("Welcome")
+                return redirect(url_for('auth.dashboard'))
         else:
-            user.authenticated = True
-            db.session.add(user)
-            db.session.commit()
-            login_user(user)
-            flash("Welcome")
-            return redirect(url_for('auth.dashboard'))
-    else:
-        flash("Invalid username or password")
+            flash('Invalid username or password')
     return render_template('login.html', form=form)
 
 
@@ -36,19 +37,19 @@ def register():
     if current_user.is_authenticated:
         return redirect(url_for('auth.dashboard'))
     form = register_form()
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user is None:
-            user = User(email=form.email.data, password=generate_password_hash(form.password.data))
-            db.session.add(user)
-            db.session.commit()
-            flash('Congratulations, you are now a registered user!', "success")
-            return redirect(url_for('auth.login'))
-        else:
-            flash('Already Registered')
-            return redirect(url_for('auth.login'))
-    elif not form.validate_on_submit():
-        flash('Already Registered')
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            user = User.query.filter_by(email=form.email.data).first()
+            if user is None:
+                user = User(email=form.email.data, password=generate_password_hash(form.password.data))
+                db.session.add(user)
+                db.session.commit()
+                flash('Congratulations, you are now a registered user!', "success")
+                return redirect(url_for('auth.login'))
+            else:
+                flash('Already Registered')
+                return redirect(url_for('auth.login'))
+    return render_template('register.html', form=form)
 
 
 @auth.route('/dashboard')
