e39fb281c471834ec4291497405835e54eb14bc6
Johnny Pillacela <johnnybelesaca@gmail.com>
2022-04-22 11:26:36 -0400

    changed for maps

--- app/__init__.py
+++ app/__init__.py
@@ -17,7 +17,6 @@ from app.db.models import User
 from app.error_handlers import error_handlers
 from app.logging_config import log_con, LOGGING_CONFIG
 from app.simple_pages import simple_pages
-from app.songs import songs
 from app.map import map
 from app.db import database
 from flask_cors import CORS
@@ -58,7 +57,6 @@ def create_app():
     # these load functionality without a web interface
     app.register_blueprint(log_con)
     app.register_blueprint(error_handlers)
-    app.register_blueprint(songs)
     app.register_blueprint(map)
     app.context_processor(utility_text_processors)
     # add command function to cli commands
